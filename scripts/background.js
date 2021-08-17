5e30627636738f34ca1f7b83421ee4433190a69f
dmtkpv <dmtkpv@gmail.com>
2021-08-17 12:00:14 +0300

    improvements

--- scripts/background.js
+++ scripts/background.js
@@ -1,24 +1,119 @@
 // --------------------
-// Send processes
+// Set icon
 // --------------------
+
+function setIcon (stats = {}) {
+
+    const size = 16;
+    const bar = 2;
+    const margin = 1;
+    const height = size - margin * 2;
+
+    const canvas = new OffscreenCanvas(size, size);
+    const ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, size, size);
+    ctx.fillStyle = stats.active ? '#3774E0' : '#3D4043';
+
+    for (let x = margin; x < size; x += bar + margin) {
+        const h = Math.sin(x / size * Math.PI) * height;
+        const y = margin + height - h;
+        ctx.fillRect(x, y, bar, h);
+    }
+
+    return chrome.action.setIcon({
+        tabId: stats.tab,
+        imageData: ctx.getImageData(0, 0, size, size)
+    });
+
+}
+
+setIcon();
+
+
+
+// --------------------
+// Stats
+// --------------------
+
+class Stats {
+
+    static store = {};
+
+    static get (tab) {
+        return Stats.store[tab];
+    }
+
+    static put (tab, stats) {
+        Stats.store[tab] = stats;
+    }
+
+    static del (tab) {
+        delete Stats.store[tab];
+    }
+
+    constructor (tab) {
+        this.tab = tab;
+        this.active = true;
+        Stats.put(tab, this);
+        chrome.processes.getProcessIdForTab(tab, process => {
+            this.process = process;
+            this.exec();
+        });
+    }
+
+    exec () {
+        console.log('exec')
+        chrome.scripting.executeScript({
+            target: { tabId: this.tab },
+            files: ['scripts/stats.min.js', 'scripts/content.js'],
+        })
+    }
+
+    toggle () {
+        this.active = !this.active;
+        chrome.tabs.sendMessage(this.tab, { key: this.state });
+    }
+
+    get state () {
+        return this.active ? 'play' : 'stop'
+    }
+
+}
+
+
+
+// --------------------
+// Listeners
+// --------------------
+
+chrome.action.onClicked.addListener(({ id }) => {
+    let stats = Stats.get(id);
+    if (stats) stats.toggle();
+    else stats = new Stats(id);
+    setIcon(stats);
+})
+
+chrome.tabs.onRemoved.addListener(id => {
+    Stats.del(id);
+})
+
+chrome.tabs.onUpdated.addListener((id, { status }) => {
+    if (status !== 'complete') return;
+    const stats = Stats.get(id);
+    if (!stats) return;
+    if (!stats.active) return Stats.del(id);
+    stats.exec();
+    setIcon(stats);
+})
 
 chrome.processes.onUpdatedWithMemory.addListener(processes => {
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-        for (const tab of tabs) {
-            chrome.tabs.sendMessage(tab.id, processes);
+        for (const { id } of tabs) {
+            const stats = Stats.get(id);
+            if (!stats || !stats.active) continue;
+            const data = processes[stats.process];
+            chrome.tabs.sendMessage(id, { key: 'process', data });
         }
-    });
+    })
 })
-
-
-
-// --------------------
-// Get process ID
-// --------------------
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message === 'processId') {
-        chrome.processes.getProcessIdForTab(sender.tab.id, sendResponse);
-    }
-    return true
-});
--- scripts/content.js
+++ scripts/content.js
@@ -9,6 +9,7 @@ class Panel {
     constructor () {
         Object.assign(this, new Stats.Panel(...arguments));
         this.values = [];
+        this.dom.style.pointerEvents = 'none';
     }
 
     add (value) {
@@ -26,22 +27,53 @@ class Panel {
 
 
 // --------------------
-// MS
+// MS Panel
 // --------------------
 
 class MS extends Panel {
 
     constructor () {
         super('MS', ...arguments);
-        this.time = performance.now();
-        requestAnimationFrame(() => this.tick());
+        this.play();
     }
 
     tick () {
         const time = performance.now();
         this.add(time - this.time);
-        this.time = time;
-        requestAnimationFrame(() => this.tick());
+        this.play(time);
+    }
+
+    play (time) {
+        this.time = time || performance.now();
+        this.req = requestAnimationFrame(() => this.tick());
+    }
+
+    stop () {
+        cancelAnimationFrame(this.req);
+    }
+
+}
+
+
+
+// --------------------
+// Cache position
+// --------------------
+
+const storage = {
+
+    manifest: chrome.runtime.getManifest(),
+
+    getKey (suffix) {
+        return storage.manifest.name + storage.manifest.version + suffix;
+    },
+
+    get (suffix) {
+        return localStorage.getItem(storage.getKey(suffix)) || 0;
+    },
+
+    set (suffix, value) {
+        localStorage.setItem(storage.getKey(suffix), value);
     }
 
 }
@@ -69,18 +101,78 @@ for (const canvas of stats.dom.children) {
     canvas.style.display = panel ? 'block' : 'none'
 }
 
+stats.dom.addEventListener('click', event => event.stopPropagation(), true);
+stats.dom.style.left = storage.get('left') + 'px';
+stats.dom.style.top = storage.get('top') + 'px';
 document.body.appendChild(stats.dom);
 
 
 
 // --------------------
-// Processes listener
+// Drag & drop
 // --------------------
 
-chrome.runtime.sendMessage('processId', id => {
-    chrome.runtime.onMessage.addListener(processes => {
-        panels.cpu.add(processes[id].cpu);
-        panels.ram.add(processes[id].privateMemory / 1024 / 1024);
-    })
-});
+let drag = false;
 
+function minmax (value, min, max) {
+    return Math.max(min, Math.min(max, value));
+}
+
+stats.dom.addEventListener('mousedown', event => {
+    const { clientX, clientY } = event;
+    const { left, top } = stats.dom.getBoundingClientRect();
+    drag = { clientX, clientY, left, top }
+})
+
+document.addEventListener('mousemove', event => {
+    if (!drag) return;
+    const left = minmax(drag.left + event.clientX - drag.clientX, 0, window.innerWidth - stats.dom.offsetWidth);
+    const top = minmax(drag.top + event.clientY - drag.clientY, 0, window.innerHeight - stats.dom.offsetHeight)
+    stats.dom.style.left = left + 'px';
+    stats.dom.style.top = top + 'px';
+    storage.set('left', left);
+    storage.set('top', top);
+})
+
+document.addEventListener('mouseup', event => {
+    drag = false;
+})
+
+document.addEventListener('mouseleave', event => {
+    drag = false;
+})
+
+
+
+// --------------------
+// Handlers
+// --------------------
+
+function play () {
+    stats.dom.style.display = '';
+    panels.ms.play();
+}
+
+function stop () {
+    stats.dom.style.display = 'none';
+    panels.ms.stop();
+}
+
+function update (data) {
+    panels.cpu.add(data.cpu);
+    panels.ram.add(data.privateMemory / 1024 / 1024);
+}
+
+
+
+// --------------------
+// Listeners
+// --------------------
+
+chrome.runtime.onMessage.addListener(({ key, data }) => {
+    switch (key) {
+        case 'stop': return stop();
+        case 'play': return play();
+        case 'process': return update(data);
+    }
+})
