79a5f38e986ef0acab2903a19d42762a5f8bd7d4
dmtkpv <dmtkpv@gmail.com>
2021-08-24 10:25:26 +0300

    set new tab icon

--- bg.js
+++ bg.js
@@ -1,16 +1,23 @@
 import { Storage, Tabs, Icon } from './api.js'
 
+async function activate (id) {
+    const data = await Storage.get(id);
+    if (!data) return await Icon.set();
+    await Tabs.execContent(id);
+    await Icon.set(id, data);
+    await Tabs.sendMessage(id, 'config', data);
+}
+
+chrome.tabs.onCreated.addListener(async ({ id }) => {
+    await activate(id);
+})
+
 chrome.tabs.onRemoved.addListener(async id => {
     await Storage.del(id);
 })
 
 chrome.tabs.onUpdated.addListener(async (id, { status }) => {
-    if (status !== 'complete') return;
-    const data = await Storage.get(id);
-    if (!data) return;
-    await Tabs.execContent(id);
-    await Icon.set(id, data);
-    await Tabs.sendMessage(id, 'config', data);
+    if (status === 'complete') await activate(id);
 })
 
 chrome.runtime.onInstalled.addListener(async () => {
